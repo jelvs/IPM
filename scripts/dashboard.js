c101ad41f2795be3adcb95659db5c5ad4039b92b
Kimeber <Kimeber@users.noreply.github.com>
2017-11-22 11:55:13 +0000

    changes ficheiros e dashboard

---
+++ scripts/dashboard.js
@@ -0,0 +1,33 @@
+/*<tr>
+                                    <td>
+                                        <span class="glyphicon glyphicon-plus-sign"></span>
+                                    </td>
+                                    <td>Californication</td>
+                                    <td>Red Hot Chili Peppers</td>
+                                    <td>dd-mm-yyyy</td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <span class="glyphicon glyphicon-plus-sign"></span>
+                                    </td>
+                                    <td>Back in Black</td>
+                                    <td>AC/DC</td>
+                                    <td>dd-mm-yyyy</td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <span class="glyphicon glyphicon-plus-sign"></span>
+                                    </td>
+                                    <td>B.Y.O.B</td>
+                                    <td>System of a Down</td>
+                                    <td>dd-mm-yyyy</td>
+                                </tr>*/
+
+$(document).ready(function(){
+    swal({
+      title: 'Swal implemented!',
+      text: 'Do you want to continue?',
+      type: 'success',
+      confirmButtonText: 'Cool'
+    })
+});
--- scripts/player.js
+++ scripts/player.js
@@ -1,17 +1,32 @@
 $(document).ready(function(){
+    function song(name, artist, album, duration, dateAdded, file) {
+        this.name = name;
+        this.artist = artist;
+        this.album = album;
+        this.duration = duration;
+        this.dateAdded = dateAdded;
+        this.file = file;
+    }
+    var sources = [];
+    sources.push(new song("Californication", "Red Hot Chilli Peppers", "Californication", "5:20", new Date(2017,5,24), "Californication_RHCP.mp3"))
+    sources.push(new song("Back in Black", "AC/DC", null, "4:15", new Date(2017,1,10), "BackInBlack_ACDC.mp3"))
+    var audio = document.getElementById("player");
     //
     $("#play").click(function(e){
         $("#pause").removeClass("invisible");
         $("#play").addClass("invisible");
+        audio.play();
     });
 
     $("#stop").click(function(e){
         $("#pause").addClass("invisible");
         $("#play").removeClass("invisible");
+        audio.stop();
     });
 
     $("#pause").click(function(e){
         $("#play").removeClass("invisible");
         $("#pause").addClass("invisible");
+        audio.pause();
     });
 });
