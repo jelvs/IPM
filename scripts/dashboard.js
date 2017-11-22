5e272a01646ead00dc51aaddbbef634fb952092a
Kimeber <Kimeber@users.noreply.github.com>
2017-11-22 15:59:52 +0000

    Add files via upload

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,33 +1,41 @@
-/*<tr>
-                                    <td>
-                                        <span class="glyphicon glyphicon-plus-sign"></span>
-                                    </td>
-                                    <td>Californication</td>
-                                    <td>Red Hot Chili Peppers</td>
-                                    <td>dd-mm-yyyy</td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <span class="glyphicon glyphicon-plus-sign"></span>
-                                    </td>
-                                    <td>Back in Black</td>
-                                    <td>AC/DC</td>
-                                    <td>dd-mm-yyyy</td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <span class="glyphicon glyphicon-plus-sign"></span>
-                                    </td>
-                                    <td>B.Y.O.B</td>
-                                    <td>System of a Down</td>
-                                    <td>dd-mm-yyyy</td>
-                                </tr>*/
-
 $(document).ready(function(){
-    swal({
-      title: 'Swal implemented!',
-      text: 'Do you want to continue?',
-      type: 'success',
-      confirmButtonText: 'Cool'
-    })
+    var user = "";
+    
+    function song(name, artist, album, duration, dateAdded, file) {
+        this.name = name;
+        this.artist = artist;
+        this.album = album;
+        this.duration = duration;
+        this.dateAdded = dateAdded;
+        this.file = file;
+    }
+    
+    var currentPlayingID = 0;
+    
+    function getRow4Table(data){
+        var btn = "<button class='btn btn-link btn-sm material_btn material_btn-link' id="+data.name+ " style='padding:7px;'><i class='fa fa-fw fa-lg fa-bars text-info'</i></button>";
+        $('.btn-link').on('click', function(){
+            console.log("Deu botão", this);
+            swal(
+              'Error',
+              'Not Implemented yet!',
+              'error'
+            )
+        })
+        $row = $('<tr><td id="btn_'+data.name+'">'+btn+'</td><td>'+ data.name+'</td><td>'+ data.artist+'</td><td>'+ data.album+'</td><td>'+ data.duration+'</td><td>'+ data.dateAdded+'</td></tr>');
+        return $row;
+    }
+    
+    var sources = [];
+    sources.push(new song("Californication", "Red Hot Chilli Peppers", "Californication", "5:20", new Date(2017,5,24), "Californication_RHCP.mp3"));
+    sources.push(new song("Back in Black", "AC/DC", null, "4:15", new Date(2017,1,10), "BackInBlack_ACDC.mp3"));
+    
+    
+    function fillTable(){
+        for (x in sources){
+            $('#TablePlaylistSongs').append(getRow4Table(sources[x]));
+        }
+    }
+    
+    fillTable();
 });
