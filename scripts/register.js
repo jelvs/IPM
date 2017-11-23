ed8cc1dfc7ab143fef45f0e517d4ccf6674bbc27
Rui Mendes <rui.mendes.95@hotmail.com>
2017-11-23 13:20:49 +0000

    Add favicon and register.js changes

--- scripts/register.js
+++ scripts/register.js
@@ -1,5 +1,6 @@
-$(document).ready(function(){
-    function user(username, email, pwd, pwd2, birth) {
+$(document).ready(function () {
+    function user(pic, username, email, pwd, pwd2, birth) {
+        this.pic = pic;
         this.username = username;
         this.email = email;
         this.pwd = pwd;
@@ -7,20 +8,58 @@ $(document).ready(function(){
         this.birth = birth;
     }
     var accounts = [];
-    accounts.push(new user("musicflow", "musicflow@gmail.com", "xpto1234", "xpto1234", null))
+    accounts.push(new user(null, "musicflow", "musicflow@gmail.com", "xpto1234", "xpto1234", null))
 
-    $("input").keyup(function(){
+    $("input").keyup(function () {
         var input = $("input").val();
     });
 
-    $("button").click(function(){
+    $("button").click(function () {
         $.post("demo_test_post.asp",
-        {
-          name: "Donald Duck",
-          city: "Duckburg"
-        },
-        function(data,status){
-            alert("Account successfully created!");
-        });
+            {
+                name: "Donald Duck",
+                city: "Duckburg"
+            },
+            function (data, status) {
+                alert("Account successfully created!");
+            });
     });
 });
+
+
+function submitRegister() {
+    document.getElementById("registerForm").submit();
+    alert(document.registerForm.username);
+}
+
+
+function registerValidation() {
+    {
+        var uid = document.registration.userid;
+        var passid = document.registration.passid;
+        var uname = document.registration.username;
+        var uadd = document.registration.address;
+        var ucountry = document.registration.country;
+        var uzip = document.registration.zip;
+        var uemail = document.registration.email;
+        var umsex = document.registration.msex;
+        var ufsex = document.registration.fsex;
+
+        alert('Form Succesfully Submitted');
+        window.location.reload()
+        return true;
+    }
+}
+
+
+function ValidateEmail(email) {
+    var mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (email.value.match(mailFormat)) {
+        return true;
+    }
+    else {
+        alert("You have entered an invalid email address!");
+        email.focus();
+        return false;
+    }
+}  
