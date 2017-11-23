b499fa323a3dd8ef357f64937c17cada2d049cd7
Rui Mendes <rui.mendes.95@hotmail.com>
2017-11-23 20:07:29 +0000

    register.js

--- scripts/register.js
+++ scripts/register.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
-    function user(pic, username, email, pwd, pwd2, birth) {
+    function user(pic, username, email, password, password2, birth) {
         this.pic = pic;
         this.username = username;
         this.email = email;
-        this.pwd = pwd;
-        this.pwd2 = pwd2;
+        this.password = password;
+        this.password2 = password2;
         this.birth = birth;
     }
     var accounts = [];
-    accounts.push(new user(null, "musicflow", "musicflow@gmail.com", "xpto1234", "xpto1234", null))
+    accounts.push(new user(null, "José", "jose@gmail.com", "test123", "test123", null))
 
     $("input").keyup(function () {
         var input = $("input").val();
@@ -17,8 +17,7 @@ $(document).ready(function () {
     $("button").click(function () {
         $.post("demo_test_post.asp",
             {
-                name: "Donald Duck",
-                city: "Duckburg"
+                
             },
             function (data, status) {
                 alert("Account successfully created!");
@@ -35,31 +34,51 @@ function submitRegister() {
 
 function registerValidation() {
     {
-        var uid = document.registration.userid;
-        var passid = document.registration.passid;
-        var uname = document.registration.username;
-        var uadd = document.registration.address;
-        var ucountry = document.registration.country;
-        var uzip = document.registration.zip;
-        var uemail = document.registration.email;
-        var umsex = document.registration.msex;
-        var ufsex = document.registration.fsex;
+        var username = document.registerForm.username;
+        var email = document.registerForm.email;
+        var password = document.registerForm.password;
+        var password2 = document.registerForm.password2;
 
-        alert('Form Succesfully Submitted');
-        window.location.reload()
-        return true;
+        if (validateUser(username)) {
+            if (validateEmail(email)) {
+                if (validatePassword(password, password2)) {
+                    alert('Form Succesfully Submitted');
+                    window.location.reload()
+                    return true;
+                }
+            }
+        }
     }
 }
 
+function validateUser(username) {
+    var existent;
+    if (username == existent) {
+        alert("It seems the username is already in use :(");
+        //username.focus();
+        return false;
+    }
+    return true;
+}
 
-function ValidateEmail(email) {
+function validateEmail(email) {
     var mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if (email.value.match(mailFormat)) {
         return true;
     }
     else {
-        alert("You have entered an invalid email address!");
-        email.focus();
+        alert("It seems the email is invalid :(");
+        //email.focus();
         return false;
     }
-}  
+}
+
+function validatePassword(password, password2) {
+
+    if (password === password2) {
+        alert("Passwords don't match");
+        //password.focus()
+        return false;
+    }
+    return true;
+}
