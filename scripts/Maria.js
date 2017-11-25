f2b3f88ff0a118528ba51a809e75634238d19697
Kimeber <Kimeber@users.noreply.github.com>
2017-11-25 22:38:02 +0000

    adicionado files cenarios

---
+++ scripts/Maria.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    var getUrlParameter = function getUrlParameter(sParam) {
+        var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : sParameterName[1];
+            }
+        }
+    };
+    var user = getUrlParameter('username');
+    var profileActive = false;
+    if (user == "Maria"){
+        profileActive = true;
+    }
+    
+    function fillPlaylist(){
+        $("#playlists").html('<p>Rock <a href="#" class="notImplemented">Add To...</a></p> <p>My Fav Music <a href="#" class="notImplemented">Add To...</a></p><p>TOP <a href="#" class="notImplemented">Add To...</a></p>')
+    }
+    
+    function fill(){
+        if (profileActive){
+            fillPlaylist();
+        }
+    }
+    fill();
+});
--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,7 +1,21 @@
 $(document).ready(function(){
     $('[data-toggle="popover"]').popover();
-    var user = "";
+    var getUrlParameter = function getUrlParameter(sParam) {
+        var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
 
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : sParameterName[1];
+            }
+        }
+    };
+    var user = getUrlParameter('username');
+    $("#loggedUser").html(user);
     function song(name, artist, album, duration, dateAdded, file) {
         this.name = name;
         this.artist = artist;
@@ -95,7 +109,7 @@ $(document).ready(function(){
     }
     $("#newSpeaker").click(function(e){
         e.preventDefault();
-        const {value: name} = swal({
+        var name = swal({
           title: 'New Speaker',
           input: 'text',
           inputPlaceholder: 'Enter the name of speaker',
